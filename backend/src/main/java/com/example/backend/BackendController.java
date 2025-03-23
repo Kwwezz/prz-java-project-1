cd69d91883e3b8b1c81cb05dc5ce7d4d3a4b7b0d
Kwwezz <krystianglod6@gmail.com>
2025-03-24 00:13:59 +0100

    Init

---
+++ backend/src/main/java/com/example/backend/BackendController.java
@@ -0,0 +1,15 @@
+package com.example.backend;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BackendController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
