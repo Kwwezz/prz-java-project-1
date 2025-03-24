26fdc571ed08a9f40c221ef4dcb2c363477ca5c9
Kwwezz <krystianglod6@gmail.com>
2025-03-24 20:38:15 +0100

    Frontend Tree View, transform data for UI form

--- backend/src/main/java/com/example/backend/BackendController.java
+++ backend/src/main/java/com/example/backend/BackendController.java
@@ -7,6 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BackendController {
 
+
+
+	// TaskGroup
+	// id customerId name createdAt, updatedAt
+	//
+
+	// Task
+	// id uuid parentId taskGroupId name plannedEndDate status: (New, In Progress, Completed Cancelled)
+	//
+
+	// Operation ...
+
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
--- frontend/src/app/pages/uikit/treedemo.ts
+++ frontend/src/app/pages/uikit/treedemo.ts
@@ -5,69 +5,175 @@ import { FormsModule } from '@angular/forms';
 import { TreeTableModule } from 'primeng/treetable';
 import { CommonModule } from '@angular/common';
 import { NodeService } from '../service/node.service';
+import { TagModule } from 'primeng/tag';
+import { DatePicker } from 'primeng/datepicker';
+import { Select } from 'primeng/select';
+import { Button } from 'primeng/button';
 
 @Component({
     selector: 'app-tree-demo',
     standalone: true,
-    imports: [CommonModule, FormsModule, TreeModule, TreeTableModule],
+    imports: [CommonModule, FormsModule, TreeModule, TreeTableModule, TagModule, DatePicker, Select, Button],
     template: `
-        <div class="card">
-            <div class="font-semibold text-xl">Tree</div>
-            <p-tree [value]="treeValue" selectionMode="checkbox" [(selection)]="selectedTreeValue"></p-tree>
-        </div>
+
 
         <div class="card">
-            <div class="font-semibold text-xl mb-4">TreeTable</div>
-            <p-treetable [value]="treeTableValue" [columns]="cols" selectionMode="checkbox" [(selectionKeys)]="selectedTreeTableValue" dataKey="key" [scrollable]="true" [tableStyle]="{ 'min-width': '50rem' }">
+            <div class="font-semibold text-xl mb-4">Tasks</div>
+            <p-treetable [value]="treeTableValue" [columns]="cols" 
+                         dataKey="key" [scrollable]="false" [tableStyle]="{ 'min-width': '50rem', 'min-height': '8rem'}">
                 <ng-template #header let-columns>
                     <tr>
-                        <th *ngFor="let col of columns">
+                        <!-- <th *ngFor="let col of columns">
                             {{ col.header }}
+                        </th> -->
+
+                        <th>
+                            <div class="flex items-center gap-1">
+                                <i class="pi pi-file"></i>
+                                <span>Name</span>                                
+                            </div>
                         </th>
+                        <th>
+                            <div class="flex items-center gap-1">
+                                <i class="pi pi-book"></i>
+                                <span>Description</span>                                
+                            </div>
+                        </th>
+                        <th>
+                            <div class="flex items-center gap-1">
+                                <i class="pi pi-calendar-clock"></i>
+                                <span>Planned Completion Date</span>                                
+                            </div>
+                        </th>               
+                        <th>
+                            <div class="flex items-center gap-1">
+                                <i class="pi pi-paperclip"></i>
+                                <span>Status</span>                         
+                            </div>
+                        </th>  
+
                     </tr>
                 </ng-template>
                 <ng-template #body let-rowNode let-rowData="rowData" let-columns="columns">
-                    <tr [ttRow]="rowNode" [ttSelectableRow]="rowNode">
-                        <td *ngFor="let col of columns; let i = index">
-                            <p-treeTableToggler [rowNode]="rowNode" *ngIf="i === 0" />
-                            <p-treeTableCheckbox [value]="rowNode" *ngIf="i === 0" />
-                            {{ rowData[col.field] }}
+                    <tr [ttRow]="rowNode">
+                        <td *ngFor="let col of columns; let i = index;">
+                            <div *ngIf="i === 0" style="width: 340px">
+                                <p-treeTableToggler [rowNode]="rowNode" *ngIf="i === 0" />
+                                <input class="p-1" pInputText type="text" [value]="rowData[col.field]" placeholder="Default" />
+                            </div>
+
+                            <div *ngIf="i === 1" >
+                                <textarea pTextarea placeholder="Your Message" [value]="rowData[col.field]" rows="2" cols="25"></textarea>
+                            </div>
+
+                            <div *ngIf="i === 2" >
+                                <p-datepicker [showIcon]="true" [showButtonBar]="true" dateFormat="dd.mm.yy" [(ngModel)]="rowData[col.field]"></p-datepicker>
+                            </div>
+
+                            <div *ngIf="i === 3" >
+                                <p-select  [(ngModel)]="rowData[col.field]" [options]="['New', 'In Progress', 'Completed', 'Cancelled']" 
+                                placeholder="Status..." />  
+                            </div>
+
+                            <div *ngIf="i == 4" class="flex gap-2">
+                                <p-button icon="pi pi-chevron-right" rounded="true"/>
+                                <p-button icon="pi pi-chevron-down" rounded="true"/>
+                                <p-button icon="pi pi-trash" rounded="true" severity="danger" />
+                            </div>
+
                         </td>
+                        
                     </tr>
                 </ng-template>
             </p-treetable>
         </div>
+      
     `,
     providers: [NodeService]
 })
 export class TreeDemo implements OnInit {
-    treeValue: TreeNode[] = [];
+
+
+    data: any[] = [
+        {
+            name: 'Konfiguracja systemu',
+            plannedEndDate: new Date(),
+            description: 'Ogólna konfiguracja i ustawienia systemu.',
+            status: 'New',
+            children: [
+                {
+                    name: 'Instalacja środowiska',
+                    plannedEndDate: new Date(),
+                    description: 'Instalacja oraz konfiguracja niezbędnego oprogramowania.',
+                    status: 'In Progress',
+                    children: [
+                        {
+                            name: 'Negocjacja z klientem',
+                            plannedEndDate: new Date(),
+                            description: 'Omówienie warunków projektu z klientem.',
+                            status: 'Cancelled'
+                        },
+                        {
+                            name: 'Negocjacja z klientem',
+                            plannedEndDate: new Date(),
+                            description: 'Zamknięcie negocjacji i zatwierdzenie warunków.',
+                            status: 'Completed'
+                        }, 
+                        {
+                            name: 'Planowanie infrastruktury',
+                            plannedEndDate: new Date(),
+                            description: 'Przygotowanie planu infrastruktury technicznej.',
+                            status: 'In Progress'
+                        }
+                    ]
+                },
+                {
+                    name: 'Wdrażanie pracowników do systemu',
+                    plannedEndDate: new Date(),
+                    description: 'Szkolenie i wprowadzenie pracowników do nowego systemu.',
+                    status: 'In Progress'
+                },
+                {
+                    name: 'Spotkanie zarządu',
+                    plannedEndDate: new Date(),
+                    description: 'Omówienie postępów i kluczowych decyzji projektowych.',
+                    status: 'New'
+                }
+            ]
+        },
+    ]
+
+    convertTasksToUITreeForm(data: any[]) {
+        return data.map(item => {
+            const { children, ...data } = item;
+            const result: any = { data };
+    
+            if (children && Array.isArray(children)) {
+                result.children = this.convertTasksToUITreeForm(children);
+            }
+    
+            return result;
+        });
+    }
+
 
     treeTableValue: TreeNode[] = [];
 
-    selectedTreeValue: TreeNode[] = [];
 
-    selectedTreeTableValue = {};
 
-    cols: any[] = [];
-
+    cols: any[] = [
+        { field: 'name', header: 'Name' },
+        { field: 'description', header: 'Description' },
+        { field: 'plannedEndDate', header: 'Planned End Date' },
+        { field: 'status', header: 'Status' },
+        { field: '', header: '' },
+    ];
     nodeService = inject(NodeService);
 
     ngOnInit() {
-        this.nodeService.getFiles().then((files) => (this.treeValue = files));
-        this.nodeService.getTreeTableNodes().then((files: any) => (this.treeTableValue = files));
+        
+        //this.nodeService.getTreeTableNodes().then((files: any) => (this.treeTableValue = files));
+        this.treeTableValue = this.convertTasksToUITreeForm(this.data);
 
-        this.cols = [
-            { field: 'name', header: 'Name' },
-            { field: 'size', header: 'Size' },
-            { field: 'type', header: 'Type' }
-        ];
-
-        this.selectedTreeTableValue = {
-            '0-0': {
-                partialChecked: false,
-                checked: true
-            }
-        };
     }
 }
